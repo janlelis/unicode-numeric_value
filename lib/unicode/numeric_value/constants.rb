80184a4891cabf95f390eeafa64dcba2f9af8ea5
Jan Lelis <hi@ruby.consulting>
2021-09-10 09:02:18 +0200

    Update Unicode version number

--- lib/unicode/numeric_value/constants.rb
+++ lib/unicode/numeric_value/constants.rb
@@ -3,7 +3,7 @@
 module Unicode
   module NumericValue
     VERSION = "1.7.0"
-    UNICODE_VERSION = "13.0.0"
+    UNICODE_VERSION = "14.0.0"
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + "/../../../data/").freeze
     INDEX_FILENAME = (DATA_DIRECTORY + "/numeric_value.marshal.gz").freeze
   end
