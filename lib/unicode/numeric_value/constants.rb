25a28e672eda09ad81c27e23de80135961e94582
Jan Lelis <hi@ruby.consulting>
2021-09-15 12:24:20 +0200

    Release v1.8.0

--- lib/unicode/numeric_value/constants.rb
+++ lib/unicode/numeric_value/constants.rb
@@ -2,7 +2,7 @@
 
 module Unicode
   module NumericValue
-    VERSION = "1.7.0"
+    VERSION = "1.8.0"
     UNICODE_VERSION = "14.0.0"
     DATA_DIRECTORY = File.expand_path(File.dirname(__FILE__) + "/../../../data/").freeze
     INDEX_FILENAME = (DATA_DIRECTORY + "/numeric_value.marshal.gz").freeze
